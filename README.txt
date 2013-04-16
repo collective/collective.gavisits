920067c50172ce9e008fb4e22d781ef2ae74c3c1
Franco Pellegrini <frapell@gmail.com>
2013-04-16 13:08:26 -0300

    Initial package structure

---
+++ README.txt
@@ -0,0 +1,20 @@
+.. contents::
+
+Introduction
+============
+
+This product provides a Dexterity behavior that will add the functionality
+of a visit counter for content types that provide it.
+It depends on collective.googleanalytics for the connection between Plone
+and Google Analytics
+It will add a new index to the catalog where the visits value will be
+updated.
+
+Usage
+=====
+
+Just install the product, and go to the Dexterity control panel and enable
+the "Visits counter" behavior for your content types.
+Make sure you have your Google Analytics authorized under the "Google 
+analytics" control panel, and the correct profile selected under the
+"reports" tab.
---
+++ setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+long_description = (
+    open('README.txt').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(name='collective.gavisits',
+      version=version,
+      description="",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://svn.plone.org/svn/collective/',
+      license='gpl',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'plone.autoform>=1.2',
+          'plone.app.dexterity',
+          'collective.googleanalytics',
+      ],
+      extras_require={
+          'test': [
+              'plone.app.testing',
+          ],
+      },
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
